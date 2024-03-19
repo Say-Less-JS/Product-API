7ed6dbd8f66690dd7707fe12e66aa2f7303e3f99
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-03-18 18:23:34 -0700

    built basic server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //server
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
