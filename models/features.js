1caaebee580c68eae73bf528bf0c2c586ae54797
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-03-22 15:53:20 -0700

    wrote model functions connecting to individual tables

--- models/features.js
+++ models/features.js
@@ -1,5 +1,15 @@
 const db = require('../db/postgres.js');
 
-module.exports = (callback) => {
+module.exports = (id, callback) => {
+  let query = 'SELECT feature, value FROM features WHERE product_id = $1';
+  let value = [id];
 
+  db.query(query, value)
+    .then((response) => {
+      callback(null, response.rows);
+    })
+    .catch((err) => {
+      console.log('Error retrieving product information');
+      callback(err);
+    });
 };
--- models/styles.js
+++ models/styles.js
@@ -1,5 +1,15 @@
 const db = require('../db/postgres.js');
 
-module.exports = (callback) => {
+module.exports = (id, callback) => {
+  let query = 'SELECT id, name, sale_price, original_price, default_style FROM related WHERE style_id = $1';
+  let value = [id];
 
+  db.query(query, value)
+    .then((response) => {
+      callback(null, response.rows);
+    })
+    .catch((err) => {
+      console.log('Error retrieving product information');
+      callback(err);
+    });
 };
