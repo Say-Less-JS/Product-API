85e4ca2084c79a955b846504aff0b96e92cc6314
Wyatt Grayson Fulbright <wgfulbright@gmail.com>
2024-03-26 16:59:16 -0700

    updated all endpoints for further testing after first success

--- index.js
+++ index.js
@@ -14,24 +14,29 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/product:id', (req, res) => {
-  let id = req.params.id.slice(1);
-  product(req, res, id);
-});
+// app.get('/product:id', (req, res) => {
+//   let id = req.params.id.slice(1);
+//   product(req, res, id);
+// });
 app.get('/product', (req, res) => {
   product(req, res, req.query.id);
 });
-app.get('/styles:id', (req, res) => {
-  let id = req.params.id.slice(1);
+// app.get('/styles:id', (req, res) => {
+//   let id = req.params.id.slice(1);
+//   styles(req, res, id);
+// });
+app.get('/styles', (req, res) => {
+  let id = req.query.id;
   styles(req, res, id);
 });
-app.get('/related:id', (req, res) => {
-  let id = req.params.id.slice(1);
+// app.get('/related:id', (req, res) => {
+//   let id = req.params.id.slice(1);
+//   related(req, res, id);
+// });
+app.get('/related', (req, res) => {
+  let id = req.query.id;
   related(req, res, id);
 });
-app.get('/prods', (req, res) => {
-  products(req, res);
-})
 app.get('/loaderio-5cd3c632d2e3afb8f1d0d6d24a4ca8df.txt', (req, res) => {
   res.send('loaderio-5cd3c632d2e3afb8f1d0d6d24a4ca8df');
 });
